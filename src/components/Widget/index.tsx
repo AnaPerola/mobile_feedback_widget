2f4a776132f58508b8311090e725ab91c71a1661
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 22:59:04 -0300

    Add icon in widget

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,10 +1,19 @@
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from 'phosphor-react'
+
 import { styles } from "./styles";
+import { theme } from "../../theme";
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <>
+      <TouchableOpacity>
+        <ChatTeardropDots 
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+    </>
   )
 }
