dab631aa026adca8ed098dfe24772ce024204d50
Ana Perola <anaperolinha@hotmail.com>
2022-05-09 23:28:06 -0300

    Fix lib old for new lib expo fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 
-import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { theme } from './src/theme';
 import { Widget } from './src/components/Widget';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
-    Inter_500Medium
+    Inter_400Regular,
+    Inter_500Medium,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <View style={{
