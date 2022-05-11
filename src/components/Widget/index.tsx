d99aced8b0d13a93a76dbf97a1c83588a9dc6f99
Ana Perola <anaperolinha@hotmail.com>
2022-05-11 19:11:37 -0300

    Add new functions/imports for components to work

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,18 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { ChatTeardropDots} from 'phosphor-react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import BottomSheet from '@gorhom/bottom-sheet';
+import { ChatTeardropDots} from 'phosphor-react-native';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+
 import { Options } from '../Options';
+import { Form } from '../Form';
+import { Success } from '../Success';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSend, setFeedbackSend] = useState(false);
+
   const bottomSheetRef= useRef<BottomSheet>(null)
 
   function handleOpen(){
     bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback(){
+    setFeedbackSend(false);
+    setFeedbackType(null);
+  }
+
+  function handleRestartSent(){
+    setFeedbackSend(true);
   }
 
   return (
@@ -30,7 +50,25 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        {
+          feedbackSend ?
+            <Success 
+              onSendAnotherFeedback={handleRestartFeedback}
+            />
+            :
+            <>
+              {
+                feedbackType ? 
+                <Form 
+                  feedbackType={feedbackType}
+                  onFeedbackCanceled={handleRestartFeedback}
+                  onFeedbackSend={handleRestartSent}
+                />
+                :
+                <Options onFeedbackTypeChanged={setFeedbackType} />
+              }
+            </>
+        }
       </BottomSheet>
     </>
   )
