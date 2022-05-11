8cbde577601ed8874ffc8c52458ead5f53fc6ca3
Ana Perola <anaperolinha@hotmail.com>
2022-05-11 19:07:41 -0300

    Add content and styles for component Options

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,10 +1,35 @@
-import { View } from "react-native";
-import { Copyright } from "../Copyright";
+import { Text, View } from "react-native";
 import { styles } from "./styles";
 
-export function Options(){
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { Copyright } from "../Copyright";
+import { Option } from "../Option";
+import { FeedbackType } from "../Widget";
+
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({onFeedbackTypeChanged}: Props){
   return (
   <View style={styles.container}>
+    <Text style={styles.title}>
+      Deixe seu feedback
+    </Text>
+    <View style={styles.options}>
+      {
+        Object
+        .entries(feedbackTypes)
+        .map(([key, value]) => (
+          <Option 
+            key={key}
+            title={value.title}
+            image={value.image}
+            onPress={()=> onFeedbackTypeChanged(key as FeedbackType)}
+          />
+        ))
+      }
+    </View>
     <Copyright />
   </View>
   )
